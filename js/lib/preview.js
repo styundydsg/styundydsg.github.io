8e3826a746e87fdf706950e2af7c0009c8d3c998
Jason <2983105040@qq.com>
2026-09-02 13:22:45 +0800

    Site updated: 2026-09-02 13:22:37

--- js/lib/preview.js
+++ js/lib/preview.js
@@ -10,12 +10,16 @@ mixins.preview = {
             let preview = this.$refs.preview,
                 content = this.$refs.previewContent;
             let images = document.querySelectorAll("img");
-            for (let i of images)
+            for (let i of images) {
+                // 跳过导航/图标类图片（如图标链接里的 gitcode、文章里带链接的图），
+                // 避免点击它们时先弹出全屏预览、随浏览器前进/后退缓存残留。
+                if (i.closest("a")) continue;
                 i.addEventListener("click", () => {
                     content.alt = i.alt;
                     content.src = i.src;
                     this.previewShow = true;
                 });
+            }
             preview.addEventListener("click", () => {
                 this.previewShow = false;
             });
